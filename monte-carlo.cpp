783f317d03ee0f11e43f0bfb8d284c9f24c6bd45
Andrew Cheng <andyhero@login40.chn.perlmutter.nersc.gov>
2024-04-24 14:02:11 -0700

    got compiling

--- monte-carlo.cpp
+++ monte-carlo.cpp
@@ -1,62 +1,33 @@
 #include <iostream>
-#include "hls_math.h"
 #include "monte-carlo.h"
 
-int num_sims = 1000000;      // Number of simulated asset paths
-theta_type S = 100.0;        // Option price
-theta_type K = 100.0;        // Strike price
-theta_type r = 0.05;         // Risk-free rate (5%)
-theta_type v = 0.2;          // Volatility of the underlying (20%)
-theta_type T = 1.0;          // One year until expiry
+// ap_uint<32> lfsr1 = 0xdcba;  // Initial seed
+// ap_uint<32> lfsr2 = 1234;  // Initial seed
 
-void dut(hls::stream<bit32_t> &strm_in, hls::stream<bit32_t> &strm_out) {
-  // Read the results from the module
-  int dummy = strm_in.read();
-
-  result_type result;
-  monte_carlo_both_price(result);
-  theta_type call = result.call;
-  theta_type put = result.put;
-
-  union { float fval; int ival; } ucall;
-  union { float fval; int ival; } uput;
-
-  ucall.fval = call;
-  uput.fval = put;
-
-  int icall = ucall.ival;
-  int iput = uput.ival;
-
-  // write output to stream
-  strm_out.write(icall);
-  strm_out.write(iput);
-}
-
-ap_uint<32> lfsr1 = 0xdcba;  // Initial seed
-ap_uint<32> lfsr2 = 1234;  // Initial seed
-
-// PRNG - pseduo random number generator using linear feedback shift registers
-unsigned int pseudo_random(ap_uint<32>& lfsr) {
-  bool b_32 = lfsr.get_bit(32-32);
-  bool b_22 = lfsr.get_bit(32-22);
-  bool b_2 = lfsr.get_bit(32-2);
-  bool b_1 = lfsr.get_bit(32-1);
-  bool new_bit = b_32 ^ b_22 ^ b_2 ^ b_1;
-  lfsr = lfsr >> 1;
-  lfsr.set_bit(31, new_bit);
-  return lfsr.to_uint();
-}
+// // PRNG - pseduo random number generator using linear feedback shift registers
+// unsigned int pseudo_random(ap_uint<32>& lfsr) {
+//   bool b_32 = lfsr.get_bit(32-32);
+//   bool b_22 = lfsr.get_bit(32-22);
+//   bool b_2 = lfsr.get_bit(32-2);
+//   bool b_1 = lfsr.get_bit(32-1);
+//   bool new_bit = b_32 ^ b_22 ^ b_2 ^ b_1;
+//   lfsr = lfsr >> 1;
+//   lfsr.set_bit(31, new_bit);
+//   return lfsr.to_uint();
+// }
 
 constexpr theta_type rand_two_div_max = 2.0 / RAND_MAX;
 // Function to generate a random number in the range [0, 1)
 theta_type generate_rand1() {
-    theta_type casted_seed = pseudo_random(lfsr1);
+    // theta_type casted_seed = pseudo_random(lfsr1);
+    theta_type casted_seed = rand();
     return rand_two_div_max * casted_seed - 1;
 }
 
 // Function to generate a random number in the range [0, 1)
 theta_type generate_rand2() {
-    theta_type casted_seed = pseudo_random(lfsr2);
+    // theta_type casted_seed = pseudo_random(lfsr2);
+    theta_type casted_seed = rand();
     return rand_two_div_max * casted_seed - 1;
 }
 
@@ -77,8 +48,6 @@ T custom_log(const T& x)
   T term_squared = term * term;
   T numerator = term;
   T denominator = 1;
-
-  LOG_LABEL:
   for (int i = 1; i <= logTerms; i++)
   {
     result += numerator / denominator;
@@ -97,8 +66,6 @@ T custom_exp(const T& x)
   T term = 1.0;
   T factorial = 1.0;
   const int expTerms = 10;
-
-  EXP_LABEL:
   for (int i = 1; i <= expTerms; i++)
   {
     term *= x / i;
@@ -123,8 +90,6 @@ theta_type gaussian_box_muller()
   // Continue generating two uniform random variables
   // until the square of their "euclidean distance"
   // is less than unity
-
-  GAUSS_LABEL:
   for (int i = 0; i < 20; i++) {
     temp_x = generate_rand1();
     temp_y = generate_rand2();
@@ -136,7 +101,7 @@ theta_type gaussian_box_muller()
     }
   }
   
-  return x * hls::sqrt(-2 * custom_log<theta_type>(euclid_sq) / euclid_sq);
+  return x * sqrt(-2 * custom_log<theta_type>(euclid_sq) / euclid_sq);
 }
 
 // Pricing a European vanilla option with a Monte Carlo method
@@ -144,12 +109,11 @@ void monte_carlo_both_price(result_type &result)
 {
 
   const theta_type S_adjust = S * custom_exp<theta_type>(T * (r - 0.5 * v * v));
-  const theta_type sqrt_const = hls::sqrt(v * v * T);
+  const theta_type sqrt_const = sqrt(v * v * T);
   theta_type S_cur = 0.0;
   theta_type call_payoff_sum = 0.0;
   theta_type put_payoff_sum = 0.0;
   
-  GAUSS_GEN_LABEL:
   for (int i = 0; i < 1000000; i++) {
     theta_type gauss_bm = gaussian_box_muller();
     S_cur = S_adjust * custom_exp<theta_type>(sqrt_const * gauss_bm);
@@ -157,8 +121,8 @@ void monte_carlo_both_price(result_type &result)
     theta_type zero2 = 0.0;
     theta_type call_val = S_cur - K;
     theta_type put_val = K - S_cur;
-    call_payoff_sum += hls::fmax(call_val, zero1);
-    put_payoff_sum += hls::fmax(put_val, zero2);
+    call_payoff_sum += fmax(call_val, zero1);
+    put_payoff_sum += fmax(put_val, zero2);
   }
   
 
--- monte-carlo.h
+++ monte-carlo.h
@@ -1,22 +1,19 @@
-#pragma once
-
 #ifndef MONTE_H
 #define MONTE_H
 
 #include <cmath>
 #include <iostream>
 
-#include "typedefs.h"
-#include <hls_stream.h>
-
 typedef struct result_type result_type;
 
 typedef float theta_type;
-extern int num_sims;
-extern theta_type S, K, r, v, T;
 
-
-void dut(hls::stream<bit32_t> &strm_out);
+static int num_sims = 1000000;      // Number of simulated asset paths
+static theta_type S = 100.0;        // Option price
+static theta_type K = 100.0;        // Strike price
+static theta_type r = 0.05;         // Risk-free rate (5%)
+static theta_type v = 0.2;          // Volatility of the underlying (20%)
+static theta_type T = 1.0;          // One year until expiry
 
 theta_type generate_rand();
 
